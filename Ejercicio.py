fe2e05b684d1acdd8b424fcede289da0b3fa6ae1
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-07-22 09:40:52 -0600

    Creada base del sistema

---
+++ Ejercicio.py
@@ -0,0 +1,24 @@
+ventas=[]
+while True:
+    print("----Menú de ventas -----\n1. Ingresar lista de ventas\n2. Mostrar todas las ventas ingresadas\n3. Calcular las venta más alta y la mas baja\n4. Calcular promedio de ventas\n5. Contar cuantos días superaron Q1000 en ventas\n6. Buscar si una venta específica existe en la lista\n7. Clasificar cada venta\n8. Salir")
+    seleccion = input("Seleccione una opción: ")
+    match seleccion:
+        case "1":
+            pass
+        case "2":
+            pass
+        case "3":
+            pass
+        case "4":
+            pass
+        case "5":
+            pass
+        case "6":
+            pass
+        case "7":
+            pass
+        case "8":
+            print("Saliendo del sistema...")
+            break
+        case _:
+            print("Opción inválida, intente nuevamente")
